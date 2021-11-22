b60baf7408c5d48024a8998139d619d013a2bf45
chi.lam <chungchilam12@gmail.com>
2021-11-22 13:34:25 +0700

    sửa login lần 3

--- site/account/login.php
+++ site/account/login.php
@@ -1,6 +1,6 @@
 <?php
 require_once "./dao/admin_users.php";
-if(isset($_SESSION['users'])){
+if (isset($_SESSION['users'])) {
   echo '<script>window.location="./index.php";</script>';
 }
 if (isset($_POST['submit'])) {
@@ -19,13 +19,7 @@ if (isset($_POST['submit'])) {
 <main class="main">
   <nav class="collection__nav">
     <div class="collection__nav-text center-center">
-<<<<<<< HEAD
-      <h1>Đăng nhập</h1>
-      <a href="index.html" class="text__gray">Trang chủ </a><span class="text__gray">/</span>
-      <span class="text__gray">Đăng nhập</span>
-=======
       <h1>Tài khoản</h1>
->>>>>>> d74a69b5962845d9a5e966370563cc18c38f3cf6
     </div>
   </nav>
 
@@ -42,15 +36,10 @@ if (isset($_POST['submit'])) {
       <a href="?page=account&act=forgotpass" class="login__control-text">Quên mật khẩu?</a>
       <div class="login__control-button">
         <button type="submit" name="submit" class="btn">Đăng nhập</button>
-<<<<<<< HEAD
         <a href="index.php?page=account&act=register">
           <p class="login__control-text border-bot">Tạo tài khoản</p>
         </a>
         <a href="index.html" class="login__control-text">Trở về trang chủ</a>
-=======
-        <p class="login__control-text border-bot">Tạo tài khoản</p>
-        <a href="index.php" class="login__control-text">Trở về trang chủ</a>
->>>>>>> d74a69b5962845d9a5e966370563cc18c38f3cf6
       </div>
     </form>
   </div>
